26e68b639aebe2751be5686c1bcf447a3d48d3cf
rodertd2000 <zdabaghov@mail.ru>
2024-04-03 10:45:18 +0300

    with dict py

---
+++ 2610_matrix/matrix_3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findMatrix(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        frequency = {}
+
+        for i in nums:
+            if frequency.get(i, 0) >= len(res):
+                res.append([])
+            res[frequency.get(i, 0)].append(i)
+            frequency[i] = frequency.get(i, 0) + 1
+
+        return res
