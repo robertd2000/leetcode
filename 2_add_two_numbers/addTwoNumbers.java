a139a34d56a6bce3c0b5852aef572767139d0975
rodertd2000 <zdabaghov@mail.ru>
2024-04-23 16:50:12 +0300

    Java

---
+++ 2_add_two_numbers/addTwoNumbers.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode root = new ListNode(0);
+        ListNode n = root;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int val1 = 0;
+            int val2 = 0;
+
+            if (l1 != null) {
+                val1 = l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                val2 = l2.val;
+                l2 = l2.next;
+            }
+
+            int sum = val1 + val2 + carry;
+            int value = sum % 10;
+            carry = sum / 10;
+
+            ListNode head = new ListNode(value);
+            n.next = head;
+            n = n.next;
+        }
+
+        ListNode res = root.next;
+        root.next = null;
+        return res;
+    }
+}
