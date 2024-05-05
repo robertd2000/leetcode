aeea37a4b9b86a0522add29b843dda361be3baca
robertd2000 <zdabaghov@mail.ru>
2024-05-05 12:55:14 +0300

    237. Delete Node in a Linked List

---
+++ 237_delete_node_in_a_linked_list/deleteNode.go
@@ -0,0 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+ func deleteNode(node *ListNode) {
+    temp := node.Next
+
+    node.Val = temp.Val
+    node.Next = temp.Next
+
+    temp = nil
+}
