bac2e5841bd1e0efe533dda4f9340af576a64823
rodertd2000 <zdabaghov@mail.ru>
2024-05-07 09:29:59 +0300

    2816. Double a Number Represented as a Linked List

---
+++ 2816_double_a_number_represented_as_a_linked_list/doubleIt.py
@@ -0,0 +1,43 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# Double a Number Represented as a Linked List
+class Solution:
+    def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        stack = []
+        carry = 0
+
+        current = head
+
+        while head:
+            stack.append(head)
+            head = head.next
+
+        tail = None
+
+        while stack:
+            cur = stack.pop()
+            val = cur.val * 2
+
+            if val >= 10:
+                val = val % 10
+                cur.val = val + carry
+                carry = 1
+            else:
+                cur.val = val + carry
+                carry = 0
+
+            cur.next = tail
+            tail = cur
+        
+        if carry:
+            temp = tail
+            tail = ListNode(carry)
+            tail.next = temp
+
+        return tail
