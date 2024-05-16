72db43cf68eea88622a2c57628b098b0e723812f
rodertd2000 <zdabaghov@mail.ru>
2024-05-16 12:25:45 +0300

    c

---
+++ 2331_evaluate_boolean_binary_tree/evaluateTree.c
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool evaluateTree(struct TreeNode* root) {
+    if (root->val == 0 || root->val == 1) {
+        return root->val == 1;
+    } else if (root->val == 2) {
+        return evaluateTree(root->left) || evaluateTree(root->right);
+    } else if (root->val == 3) {
+        return evaluateTree(root->left) && evaluateTree(root->right);
+    }
+    return false;
+}
