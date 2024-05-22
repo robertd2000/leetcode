5c9b798b5425374ef78303f8b0e04076837ab4fd
rodertd2000 <zdabaghov@mail.ru>
2024-05-22 11:13:39 +0300

    py

---
+++ 131_palindrome_partitioning/partition.py
@@ -0,0 +1,13 @@
+from functools import cache
+
+
+class Solution(object):
+    @cache  
+    def partition(self, s):
+        if not s: return [[]]
+        ans = []
+        for i in range(1, len(s) + 1):
+            if s[:i] == s[:i][::-1]:
+                for suf in self.partition(s[i:]): 
+                    ans.append([s[:i]] + suf)
+        return ans
