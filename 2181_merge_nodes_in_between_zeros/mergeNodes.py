f283eade32ba5098a70f0fd3e196bffd113987d3
robertd2000 <zdabaghov@mail.ru>
2024-07-04 20:27:02 +0300

    py

---
+++ 2181_merge_nodes_in_between_zeros/mergeNodes.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeNodes(self, head):
+        modify = head.next  
+        next_sum = modify
+
+        while next_sum:
+            total = 0
+
+            while next_sum.val != 0:
+                total += next_sum.val
+                next_sum = next_sum.next
+
+            modify.val = total
+            next_sum = next_sum.next
+            modify.next = next_sum
+            modify = modify.next
+
+        return head.next
