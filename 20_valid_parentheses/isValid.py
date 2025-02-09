20b7628cf759c3f5e4bfe288a15b354ee2eb6a16
robert <ext-robert.dabagov@kuper.ru>
2025-02-09 22:04:56 +0300

    py

---
+++ 20_valid_parentheses/isValid.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        parentheses = {
+            ')': '(',
+            '}': '{',
+            ']': '[',
+        }
+
+        for i in s:
+            if i in parentheses:
+                if stack and stack[-1] == parentheses[i]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(i)
+        
+        if len(stack) > 0:
+            return False
+
+        return True
