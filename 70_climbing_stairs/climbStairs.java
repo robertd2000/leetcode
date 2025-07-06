99285dd95f0888e5a6d0d3d110f1ba29f152516d
robertd2000 <zdabaghov@mail.ru>
2025-07-06 20:48:30 +0300

    java

---
+++ 70_climbing_stairs/climbStairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n < 2)
+            return 1;
+
+        int curr = 1;
+        int prev = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int temp = curr;
+            curr = prev + curr;
+            prev = temp;
+        }
+
+        return curr;
+    }
+}
