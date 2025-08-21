126eae1e5a4962fd490404ef5b562e3a76320b29
robertd2000 <zdabaghov@mail.ru>
2025-08-21 11:56:15 +0300

    go

--- 217_contains_duplicate/containsDuplicate.go
+++ 217_contains_duplicate/containsDuplicate.go
@@ -1,13 +1,15 @@
+package containsduplicate
+
 func containsDuplicate(nums []int) bool {
-    count := make(map[int]int)
+	count := make(map[int]int)
 
-    for _, i := range nums {
-        if _, ok := count[i]; ok {
-            return true
-        }
+	for _, i := range nums {
+		if _, ok := count[i]; ok {
+			return true
+		}
 
-        count[i] = 1
-    }
-    
-    return false
+		count[i] = 1
+	}
+
+	return false
 }
