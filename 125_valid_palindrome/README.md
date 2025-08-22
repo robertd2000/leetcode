c4d8eee72ea440592a020455dd884d25f5c56a76
robertd2000 <zdabaghov@mail.ru>
2025-08-22 13:19:41 +0300

    go

--- 125_valid_palindrome/README.md
+++ 125_valid_palindrome/README.md
@@ -168,7 +168,41 @@ public:
 
 ```
 
-```cpp
+```go
+
+func isPalindrome(s string) bool {
+    s = strings.TrimSpace(s)
+    if s == "" {
+        return true
+    }
+
+    s = strings.ToLower(s)
+    var sb strings.Builder
+
+    for _, c := range s {
+        if c >= 'a' && c <= 'z' || c >= '0' && c <= '9' {
+            sb.WriteRune(c)
+        }
+    }
+
+    s = sb.String()
+    l, r := 0, len(s) - 1
+
+    for l < r {
+        if s[l] != s[r] {
+            return false
+        }
+
+        l++
+        r--
+    }
+
+    return true
+}
+
+```
+
+```go
 
 func isPalindrome(s string) bool {
     n := len(s)
--- 125_valid_palindrome/isPalindrome.go
+++ 125_valid_palindrome/isPalindrome.go
@@ -1,33 +1,67 @@
+// func isPalindrome(s string) bool {
+// 	n := len(s)
+// 	l, r := 0, n-1
+
+// 	for l < r {
+// 		lR, rR := rune(s[l]), rune(s[r])
+// 		if !isAlphanumeric(lR) {
+// 			l++
+// 		}
+// 		if !isAlphanumeric(rR) {
+// 			r--
+// 		}
+
+// 		if isAlphanumeric(lR) && isAlphanumeric(rR) {
+// 			if strings.ToLower(string(lR)) != strings.ToLower(string(rR)) {
+// 				return false
+// 			}
+
+// 			l++
+// 			r--
+// 		}
+// 	}
+
+// 	return true
+// }
+
+// func isAlphanumeric(ch rune) bool {
+// 	if unicode.IsLetter(ch) || unicode.IsDigit(ch) {
+// 		return true
+// 	}
+
+// 	return false
+// }
+
+package validpalindrome
+
+import "strings"
+
 func isPalindrome(s string) bool {
-	n := len(s)
-	l, r := 0, n-1
+    s = strings.TrimSpace(s)
+    if s == "" {
+        return true
+    }
 
-	for l < r {
-		lR, rR := rune(s[l]), rune(s[r])
-		if !isAlphanumeric(lR) {
-			l++
-		}
-		if !isAlphanumeric(rR) {
-			r--
-		}
+    s = strings.ToLower(s)
+    var sb strings.Builder
 
-		if isAlphanumeric(lR) && isAlphanumeric(rR) {
-			if strings.ToLower(string(lR)) != strings.ToLower(string(rR)) {
-				return false
-			}
+    for _, c := range s {
+        if c >= 'a' && c <= 'z' || c >= '0' && c <= '9' {
+            sb.WriteRune(c)
+        }
+    }
 
-			l++
-			r--
-		}
-	}
+    s = sb.String()
+    l, r := 0, len(s) - 1
 
-	return true
-}
+    for l < r {
+        if s[l] != s[r] {
+            return false
+        }
 
-func isAlphanumeric(ch rune) bool {
-	if unicode.IsLetter(ch) || unicode.IsDigit(ch) {
-		return true
-	}
+        l++
+        r--
+    }
 
-	return false
+    return true
 }
